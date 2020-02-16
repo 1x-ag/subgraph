89f5ab10ce59760a6e38155f2e81145ab43ed504
Anton Bukov <k06aaa@gmail.com>
2020-02-16 18:08:54 +0300

    First commit

---
+++ src/mapping.ts
@@ -0,0 +1,59 @@
+import { BigInt, ByteArray, Bytes, crypto } from '@graphprotocol/graph-ts';
+import { ClosePosition as ClosePositionEvent, OpenPosition as OpenPositionEvent } from '../generated/Contract/Contract';
+import { Positions } from '../generated/schema';
+
+function padHex(hex: string, length: i32): string {
+    if (hex.startsWith('0x')) {
+        hex = hex.substr(2);
+    }
+    return hex.padStart(length, '0');
+}
+
+function hashOfPosition(
+    contract: Bytes,
+    owner: Bytes
+): ByteArray {
+    return crypto.keccak256(
+        ByteArray.fromHexString(
+            padHex(contract.toHex(), 40) +
+            padHex(owner.toHex(), 40)
+        )
+    );
+}
+
+export function handleOpenPosition(event: OpenPositionEvent): void {
+    let entity_id = hashOfPosition(
+        event.address,
+        event.params.owner
+    ).toHex();
+
+    let entity = Positions.load(entity_id);
+
+    if (entity == null) {
+        entity = new Positions(entity_id);
+
+        entity.contract = event.address;
+        entity.owner = event.params.owner;
+        entity.amount = event.params.amount;
+        entity.stopLoss = event.params.stopLoss;
+        entity.takeProfit = event.params.takeProfit;
+    }
+
+    entity.save();
+}
+
+export function handleClosePosition(event: ClosePositionEvent): void {
+    let entity_id = hashOfPosition(
+        event.address,
+        event.params.owner
+    ).toHex();
+
+    let entity = Positions.load(entity_id);
+
+    if (entity == null) {
+        entity = new Positions(entity_id);
+        entity.closed = true;
+    }
+
+    entity.save();
+}
